c04bf757895c7042464d2d85c6c8087878d957df
Markuss Mišins <78017352+Fleksis@users.noreply.github.com>
2021-10-08 22:37:52 +0000

    Uzlabota versija kodam ar masīviem.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 
 int main() {
-  unsigned long long skaitlis1, skaitlis2, summa, cikluSkaits;
 
+  int masivs[20]; // masīva maksimālais elementu skaits ir 20
+  int skaits = 0;
+  std::cin >> skaits; // ievadam masīva elementu skaitu
+  for(int i = 0; i < skaits; i++) { 
+      std::cin >> masivs[i];      // aizpildam masīvu
+  }
+
+  // jūsu kods zemāk 
+
+  int skaitlis1, skaitlis2, summa, pareizs;
+  long masivs2[20];
+  
   skaitlis1 = 0;
   skaitlis2 = 1;
-  summa = 0;
+  pareizs = 0;
+  
   std::cout << "========= \n";
   std::cout << "fibonacci \n";
   std::cout << "========= \n";
 
-  std::cout << "Ievadi skaitli līdz 99, lai noteikti fibonači: ";
-  std::cin >> cikluSkaits;
+  std::cout << "Ievadi skaitli līdz 99, lai noteikti fibonači: \n";
 
-  if(cikluSkaits <= 99)
-  {
-    for (int i = 1; i < cikluSkaits+1; i++) {
-      summa = skaitlis1 + skaitlis2;
-      std::cout << i << " = ";
-      std::cout << summa << std::endl;
-      skaitlis2 = skaitlis1;
-      skaitlis1 = summa;
+  if(skaits <= 99)
+    {
+      for (int i = 0; i < skaits; i++) {
+        summa = skaitlis1 + skaitlis2;
+        skaitlis2 = skaitlis1;
+        skaitlis1 = summa;
+        masivs2[i] = summa;
+        std::cout << masivs2[i] << " = " << summa << std::endl;
+        }
     }
-  } else {
-    std::cout << "ERROR: Skaitlis tika ievadīts par lielu!";
-  }
-  return 0;
+    std::cout << "================================= \n";
+    for(int i = 0; i < skaits; i++){
+      if(masivs[i] == masivs2[i]){
+          pareizs+=1;
+        }
+    }
+    if(pareizs == skaits){
+      std::cout << "Pareizs";
+    } else {
+      std::cout << "Nepareizs";
+    }
+
+    return 0;
 }
